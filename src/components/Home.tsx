0ed0822a0e4f1f139b4511cbef4cfe1e445a1103
Pablo Jonatan <this.pablo@gmail.com>
2022-03-13 02:59:55 -0300

    feat: added animation to background

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,40 +1,57 @@
-import { AbsoluteCenter, Box, Center, Flex, Grid, VStack } from "@chakra-ui/react"
+import { usePrefersReducedMotion, AbsoluteCenter, Box, Center, Container, Flex, Grid, VStack, keyframes } from "@chakra-ui/react"
 import { Articles } from "./Articles"
 import { Main } from './Main'
 
+const change = keyframes`
+	0% {
+		background-position: 0% 50%;
+	}
+	50% {
+		background-position: 100% 50%;
+	}
+	100% {
+		background-position: 0% 50%;
+	}
+
+`;
+
 export const Home = () => {
+  const prefersReducedMotion = usePrefersReducedMotion()
+
+  const animation = prefersReducedMotion
+    ? undefined
+    : `${change} infinite 15s alternate-reverse`
   return (
-    <Flex
-      flex={1}
+    <Container
       maxW={'100%'}
-      h='100vh'
-      alignItems="center"
-      flexDirection="column"
-      pb="5px"
-      pt="5px"
-      style={{
-        backgroundColor: '#e5e5f7',
-        opacity: '0.8',
+      animation={animation}
+      sx={{
+        opacity: '1',
         backgroundImage:
-          'linear-gradient(to bottom, wheat, rgba(0,0,0,0.1) 1px), radial-gradient(circle, white 5%, wheat 25%, rgba(196,43,255,1), rgba(70,44,241,1))',
+          'linear-gradient(to bottom, wheat, rgba(0,0,0,0.1)), linear-gradient(to left, wheat, rgba(196,43,255,50), rgba(70,44,241,50))',
+        backgroundSize: "400% 400%",
       }}
     >
       <Flex
-        h='100vh'
-        bgColor='#fff5'
-        alignSelf={'center'}
+        flex={1}
+        alignItems="center"
+        flexDirection="column"
       >
-        <VStack
+        <Flex
+          h='100vh'
+          bgColor='#ffffffDB'
+          alignSelf={'center'}
         >
-          <Main />
-          <Center>
+          <VStack
+          >
+            <Main />
             <Box maxW='550px'>
               <Articles />
             </Box>
-          </Center>
-        </VStack>
-      </Flex>
-    </Flex >
+          </VStack>
+        </Flex>
+      </Flex >
+    </Container >
   )
 }
 
